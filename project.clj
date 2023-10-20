4543cd5bf1092df33906647f21ab32156b9c85b0
James Reeves <jreeves@weavejester.com>
2023-10-20 21:34:13 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.ring-clojure/ring-websocket-transit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for using the Transit data format with Ring websockets"
+  :url "https://github.com/ring-clojure/ring-websocket-transit"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
