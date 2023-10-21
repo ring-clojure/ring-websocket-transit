c615283bb3c1f80f224e4a0b627791daab4e3f5f
James Reeves <jreeves@weavejester.com>
2023-10-21 01:47:36 +0100

    Release 0.1.0-beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.ring-clojure/ring-websocket-transit "0.1.0-SNAPSHOT"
+(defproject org.ring-clojure/ring-websocket-transit "0.1.0-beta1"
   :description "Library for using the Transit data format with Ring websockets"
   :url "https://github.com/ring-clojure/ring-websocket-transit"
   :license {:name "The MIT License"
